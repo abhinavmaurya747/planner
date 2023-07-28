241fc7157a2d5e7a39e0781ddc46254a5cd1e220
Abhinav Maurya <Abhinavmaurya747@gmail.com>
2023-07-28 18:54:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 import openai
-from openai_utils import get_meal_plan
+from scheduler import *
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())  # read local .env file
 
-openai.api_key = "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"
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
-def index():
+
+@app.route('/', methods=['GET', 'POST'])
+async def index():
     if request.method == "POST":
-        user_meals = request.form['user_meals']
-        # print(user_meals)
-        meal_plan = get_meal_plan(user_meals)
-        # print(meal_plan)
-        return jsonify({"meal_plan" : meal_plan})
+        user_data = request.form
+        meal_requirements = dict(user_data)
+        print("User Sent : ", meal_requirements)
+        sch = Schedulder(meal_requirements)
+        json_data = await sch.planner()
+        left_day = await sch.find_leftover_takeout()
+        final_treats = await sch.find_treats()
+        sch.dump()
+
+        json_data = json_data[:-1] + "," + \
+            left_day[1:-1] + "," + final_treats[1:]
+
+        print(json_data)
+
+        return json_data
+
     return render_template('index.html')
 
 if __name__ == '__main__':
--- openai_utils.py
+++ openai_utils.py
@@ -1,26 +1,16 @@
 import openai
+import asyncio
 
-def get_meal_plan(user_meals, model="gpt-3.5-turbo", temperature=0, max_tokens=500):
-    messages = create_message(user_meals)
+
+async def get_completion_from_messages(messages,
+                                       model="gpt-3.5-turbo",
+                                       temperature=1,
+                                       max_tokens=3000):
+    await asyncio.sleep(1)
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
         temperature=temperature,
-        max_tokens=max_tokens
+        max_tokens=max_tokens,
     )
-    return response.choices[0].message['content']
-
-def create_message(user_meals):
-    messages = [
-        {'role' : 'system',
-         'content' : '''You are a nutrionist whose role is to understand what your patients prefers to eat or drink.
-         From the provided data you have to first find out what types of meals patient can prefer. 
-         On the basis of your understanding, create a meal plan to follow as per weekly and daily schedule. 
-         The consisting of various meals as per the user's need. 
-         Then you have also to list all the ingredient list for those meals.
-         '''},
-         {'role' : 'user',
-          'content' : user_meals
-          }
-    ]
-    return messages
+    return {"context": response.choices[0].message["content"], "usage": response.usage}
